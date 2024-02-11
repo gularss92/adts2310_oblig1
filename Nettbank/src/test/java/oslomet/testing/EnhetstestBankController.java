8d20b227acbb6fe2635649fb70404d6a348d182d
gularss92 <gunnar.larsson92@gmail.com>
2024-02-11 14:40:20 +0100

    Lagt til enhetstester for utforBetaling_LoggetInn, utforBetaling_LoggetInn_IDFeil og utforBetaling_IkkeLoggetInn.

--- Nettbank/src/test/java/oslomet/testing/EnhetstestBankController.java
+++ Nettbank/src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
