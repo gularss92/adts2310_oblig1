efcf398c5d013cf56d1fc4bd62ab5ba9ca4e9ede
gularss92 <gunnar.larsson92@gmail.com>
2024-02-21 09:35:00 +0100

    Lagt klar mocking for testing av adminkonto

---
+++ Nettbank/src/test/java/oslomet/testing/AdminKontoController.java
@@ -0,0 +1,25 @@
+package oslomet.testing;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminKontoController {
+
+    @InjectMocks
+    // denne skal testes
+    private AdminKontoController adminKontoController;
+
+    @Mock
+    // denne skal Mock'es
+    private AdminRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private Sikkerhet sjekk;
+}
+
